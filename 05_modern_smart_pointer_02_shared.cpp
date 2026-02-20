99fd052fa5306fdaac5b2885c0d168a36018e0e9
Saroj Kumar N <nsaroj.786@gmail.com>
2026-02-20 16:43:08 +0530

    Implement shared pointer example with use_count
    Demonstrate usage of std::shared_ptr with reference counting.

--- 05_modern_smart_pointer_02_shared.cpp
+++ 05_modern_smart_pointer_02_shared.cpp
@@ -1,1 +1,26 @@
 // Shared Pointer:std::shared_ptr
+/******************************************************************************
+//Shared pointer: 
+ - is a container for a raw pointer, maintains reference counting ownership of its contained pointer
+ - An object referenced by the contained raw pointer will be destroyed only when all copies of the shared pointer have been deleted.
+ - p1.use_count : to get number of referncing count
+    
+*******************************************************************************/
+
+#include <iostream>
+#include <memory> // required for unique pointers
+
+int main()
+{ 
+    std::shared_ptr<int> p1(new int(10));
+    std::shared_ptr<int> p2(p1); // two pointers pointing to the same address
+    std::cout<<"p1.use_count : "<< p1.use_count()<<std::endl; //2
+    std::cout<<"p2.use_count : "<< p2.use_count()<<std::endl; //2
+    p1.reset();
+     std::cout<<"p1.use_count : "<< p1.use_count()<<std::endl; // 0 as p1 alread set to nullptr
+    std::cout<<"p2.use_count : "<< p2.use_count()<<std::endl;
+    std::cout<< p1.get(); // 0 as nothing
+    std::cout<< p2.get(); // valid address
+    
+    return 0;
+}
